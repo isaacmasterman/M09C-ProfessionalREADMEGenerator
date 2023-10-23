1bf94c7fa48bc0965df5dff2229fd0bc788a0582
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2023-10-23 17:31:20 +1100

    creating and testing functions to generate quality README

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# M09C-ProfessionalREADMEGenerator
-This command-line application leverages the Inquirer package to dynamically generate a comprehensive and professional README.md file, streamlining the project setup process by prompting the user for essential details and automatically creating well-structured documentation.
+# Professional README Generator
+
+  ## Description
+  This command-line application simplifies the process of creating a professional README file for your projects. By answering a series of prompts about your application, a high-quality, formatted README.md file is generated, ready to be added to your repository.
+  
+  - What was your motivation? The motivation behind this project is to streamline the process of creating consistent and comprehensive README files for developers, ensuring that all necessary sections are included and formatted correctly.
+  - Why did you build this project? This tool serves to save time and increase productivity for developers, allowing them to focus more on coding and less on documentation.
+  - What problem does it solve? This tool serves to save time and increase productivity for developers, allowing them to focus more on coding and less on documentation.
+  - What did you learn? It addresses the issue of incomplete or inconsistent README files across different projects, ensuring that all necessary information is included and presented in a professional manner.
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Features](#features)
+  - [Contribute](#contribute)
+  - [Tests](#tests)
+  
+  ## Installation
+  Through the development of this application, I have enhanced my skills in Node.js, working with npm packages, and handling user input from the command line.
+  
+  ## Usage
+  Follow these steps to install and run the ProfessionalREADMEGenerator on your local machine: 1. ** Clone the Repository ** 2. ** Install the Dependencies - npm install ** 3. Run the Application - node index.js
+  
+  ## Credits
+  N/A
+  
+  ## License
+  ## License
+This project is licensed under the MIT license.
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+For more information on the license, [see here]([MIT License](https://opensource.org/licenses/MIT)).
+
+  
+  ## Features
+  N/A
+  
+  ## Contribute
+  N/A
+  
+  ## Tests
+  To run tests on the application, install the necessary dependencies and run the following command: npm test
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,91 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'GPLv3':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'Apache 2.0':
+      return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    case 'BSD 3-Clause':
+      return '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '[MIT License](https://opensource.org/licenses/MIT)';
+    case 'GPLv3':
+      return '[GNU General Public License v3](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'Apache 2.0':
+      return '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
+    case 'BSD 3-Clause':
+      return '[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {  
+  if (license === 'None') {
+    return 'This project is not licensed.';
+  }
+  return `## License
+This project is licensed under the ${license} license.
+${renderLicenseBadge(license)}
+For more information on the license, [see here](${renderLicenseLink(license)}).
+`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+  
+  - What was your motivation? ${data.motivation}
+  - Why did you build this project? ${data.problemSolved}
+  - What problem does it solve? ${data.problemSolved}
+  - What did you learn? ${data.learnings}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Features](#features)
+  - [Contribute](#contribute)
+  - [Tests](#tests)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Credits
+  ${data.credits}
+  
+  ## License
+  ${renderLicenseSection(data.license)}
+  
+  ## Features
+  ${data.features}
+  
+  ## Contribute
+  ${data.contribute}
+  
+  ## Tests
+  ${data.tests}
 `;
 }
 
